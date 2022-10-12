d7d05dc11395b33ebb3afd996eaa61ff1d1c8e6e
Jackson <pertusatti22@gmail.com>
2022-10-12 10:26:03 -0300

    Implementando componente de Text com Storybook

---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,33 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from './Text'
+
+export default {
+	title: 'Components/Text',
+	component: Text,
+	args: {
+		children: 'The quick brown fox jumps over the lazy dog.',
+	}
+} as Meta<TextProps>
+
+export const Default: StoryObj<TextProps> = {}
+
+export const Small: StoryObj<TextProps> = {
+	args: {
+		size: 'sm'
+	}
+}
+
+export const Large: StoryObj<TextProps> = {
+	args: {
+		size: 'lg'
+	}
+}
+
+export const CustomComponent: StoryObj<TextProps> = {
+	args: {
+		asChild: true,
+		children: (
+			<p>Test</p>
+		)
+	}
+}
