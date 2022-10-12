922c382900680db30dbfd3b26399d82454f97e80
Jackson <pertusatti22@gmail.com>
2022-10-12 09:14:16 -0300

    Tailwind, fontes e setup do storybook

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,6 @@ export function App() {
         Enviar
       </button>
     </>
-    
+
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.tsx',
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: 'Inter, sans-serif'
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/**/*.tsx',
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: 'Inter, sans-serif'
+			},
+		},
+	},
+	plugins: [],
 }
+
+//Storybook é uma ferramenta que permite visualizar os componentes funcionando de forma isolada.
